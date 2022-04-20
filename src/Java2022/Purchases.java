d5556b38ced9102e8cda8693b5d3efc83e9e8b9e
Evan Johnson <mr.johnson@EvLJoh.local>
2022-04-20 14:11:23 -0400

    Initial commit

---
+++ src/Java2022/Purchases.java
@@ -0,0 +1,18 @@
+package Java2022;
+
+public class Purchases {
+    private String purchItem;
+    private double purchPrice;
+    public String getPurchItem() {
+        return purchItem;
+    }
+    public void setPurchItem(String purchItem) {
+        this.purchItem = purchItem;
+    }
+    public double getPurchPrice() {
+        return purchPrice;
+    }
+    public void setPurchPrice(double purchPrice) {
+        this.purchPrice = purchPrice;
+    }
+}
---
+++ src/Java2022/inventory.java
@@ -0,0 +1,18 @@
+package Java2022;
+
+public class inventory {
+    private String item;
+    private double price;
+    public String getItem() {
+        return item;
+    }
+    public void setItem(String item) {
+        this.item = item;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
